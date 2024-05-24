44a43b9edc8a0ecdd843acdd3ed517cf3840e8b1
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2024-05-24 12:56:22 +0700

    🦋 ADD - api manga 🦋

--- src/api/manga/manga.controller.ts
+++ src/api/manga/manga.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { MangaService } from './manga.service';
-
+import { Request } from 'express';
 @Controller('manga')
 export class MangaController {
   constructor(private readonly mangaService: MangaService) {}
   @Get()
   getAllManga() {
-    return this.mangaService.getAllManga();
+    return this.mangaService.getAll();
+  }
+
+  @Get(':id')
+  getMangaById(@Req() request: Request) {
+    const { id } = request.params;
+    return this.mangaService.getMangaById(id);
   }
 }
--- src/api/manga/manga.service.ts
+++ src/api/manga/manga.service.ts
@@ -1,8 +1,36 @@
+import { LoggerService } from '@/services/loggerService/logger.service';
+import { PrismaService } from '@/services/prismaService/prisma.service';
 import { Injectable } from '@nestjs/common';
 @Injectable()
 export class MangaService {
-  constructor() {}
-  async getAllManga() {
-    console.log('Get All Manga!');
+  constructor(
+    private readonly loggerService: LoggerService,
+    private readonly prismaService: PrismaService,
+  ) {}
+  async getAll() {
+    try {
+      const mangaSources = await this.prismaService.manga_source.findMany();
+      return {
+        data: mangaSources,
+      };
+    } catch (err) {
+      this.loggerService.error(err);
+      throw new Error('Failed to fetch manga sources');
+    }
+  }
+
+  async getMangaById(id: string) {
+    try {
+      const manga = await this.prismaService.manga_source.findUnique({
+        where: { id },
+      });
+
+      return {
+        data: manga,
+      };
+    } catch (err) {
+      this.loggerService.error(err);
+      throw new Error('Failed to fetch manga sources');
+    }
   }
 }
